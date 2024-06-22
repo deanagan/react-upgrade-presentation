872e28cdf8f9eed1538d447ba8b4be6c6bc02f64
Dean Agan <agandfr@gmail.com>
2024-06-22 13:00:42 +1000

    Converting to react17. Add required projects. Also convert to typescript.

---
+++ react17demo/src/index.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App: React.FC = () => {
+  
+  return (
+    
+    <div>React 17 Demo!!</div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("react-17-demo"));
