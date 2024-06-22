084faea85312820347011b1fc2d8c2e0b3025469
Dean Agan <agandfr@gmail.com>
2024-06-22 13:03:37 +1000

    Configure webpack

--- react17demo/webpack.config.js
+++ react17demo/webpack.config.js
@@ -1,9 +1,33 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require("path");
 
 module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
   mode: "development",
   devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
     port: 8081,
   },
   plugins: [
